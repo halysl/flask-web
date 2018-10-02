d3ebb7f049de88bccedfd9cc847b4f4e9e284435
yagami light <halysl0817@gmail.com>
2018-10-02 22:37:37 +0800

    add autopep8

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8 -*-
 
-from app import app
 from flask import render_template
+
+from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'light'}
+    posts = [
+        {'author': {'nickname': 'light'},
+         'body': 'Beautiful is better than ugly.'}
+    ]
     return render_template('index.html', title='Home', user=user)
