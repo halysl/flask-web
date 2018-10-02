e60b63d03cf3972533c5463c6dad11416ae98f5f
yagami light <halysl0817@gmail.com>
2018-10-02 22:16:50 +0800

    start

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,5 @@ from flask import Flask
 
 # 创建一个Flask对象，并修改此对象的config
 app = Flask(__name__)
-app.config.from_object('config')
 
-# 从app对象导入视图方法
 from app import views
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+# -*- xodinf:utf-8 -*-
+
 from app import app
 #以debug模式启动flask框架
-app.run(debug = True)
+app.run()
