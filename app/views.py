664758b64812603814bd2c3e715357265e973b51
yagami light <halysl0817@gmail.com>
2018-10-03 11:38:05 +0800

    reciver form info

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
-from flask import render_template
+from flask import render_template, redirect, flash
 
 from app import app
+from .forms import LoginForm
 
 
 @app.route('/')
@@ -20,3 +21,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID={openid},remember_me={remember_me}'.format(
+            openid=form.openid.data, 
+            remember_me=form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
