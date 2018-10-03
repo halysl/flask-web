f3ae0fd0ab7b4d838ffcc2158c8ec55e23d30565
yagami light <halysl0817@gmail.com>
2018-10-03 18:18:08 +0800

    add db file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # 创建一个Flask对象，并修改此对象的config
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 from app import views, models
