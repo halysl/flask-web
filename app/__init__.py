88e2252930d43ab241496c435a0cdb6bd23fdbcf
yagami light <halysl0817@gmail.com>
2018-10-03 15:39:23 +0800

    add a sqlalchemy object

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # 创建一个Flask对象，并修改此对象的config
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
 from app import views
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import os
 # 防止CSRF跨站攻击
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
@@ -11,3 +12,8 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'app.db'))
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
