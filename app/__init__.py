6649e1eb081f6241dde7ceea1b4c5112e7569848
yagami light <halysl0817@gmail.com>
2018-10-03 17:12:34 +0800

    add db config

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 # 创建一个Flask对象，并修改此对象的config
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
-from app import views
+db = SQLAlchemy()
+db.init_app(app)
+
+from app import views, models
